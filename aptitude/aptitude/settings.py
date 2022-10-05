f50376e2ca2c0b9e99f3b50097938cfa1ddd0980
seoseongjo <tjtjdwh5321@gmail.com>
2022-10-05 16:34:16 +0900

    html test

--- aptitude/aptitude/settings.py
+++ aptitude/aptitude/settings.py
@@ -8,7 +8,6 @@ DATABASES = local_settings.DATABASES
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
