13e09dec475fa54bf0261012aaac5276837eb0f6
seongjo-seo <tjtjdwh5321@gmail.com>
2022-10-06 11:19:09 +0900

    불필요한 users app의 urls.py의 경로 값 제거

--- aptitude/aptitude/settings.py
+++ aptitude/aptitude/settings.py
@@ -88,8 +88,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = 'static/'
 
-# AUTH_USER_MODEL = 'users.User'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
