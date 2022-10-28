081e3672c4a21ec48f6ff7c4c591f987268205bb
seoseongjo <tjtjdwh5321@gmail.com>
2022-10-29 03:27:34 +0900

    custom orm write and database settings

---
+++ server/controllers/custom.js
@@ -0,0 +1,34 @@
+const { v4: uuidv4 } = require("uuid");
+const database = require("../models/custom");
+const custom = database.custom;
+
+// Create and Save a new Post
+exports.create = async (req, res) => {
+  console.log(req.body);
+
+  const { post, view, userId } = req.body;
+
+  if (!req.body.userId) {
+    res.status(400).send({
+      message: "ID must be entered!",
+    });
+    return;
+  }
+
+  const customData = {
+    id: uuidv4(),
+    post,
+    view,
+    userId,
+  };
+
+  try {
+    const created = await custom.create(customData);
+
+    res.send(created);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while creating the Post.",
+    });
+  }
+};
