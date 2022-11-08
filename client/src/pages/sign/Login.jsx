8f96d5b681e2978e63e058a2ed9ec60ddaad361c
seongjo-seo <tjtjdwh5321@gmail.com>
2022-11-08 18:00:19 +0900

    아이콘 사이즈 조정 필요 및 로그인 전체 임시 배치 단계 완료

--- client/src/pages/sign/Login.jsx
+++ client/src/pages/sign/Login.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useRef, useState, useEffect } from 'react';
-
+import { FaGoogle, RiKakaoTalkFill } from 'react-icons/fa';
+import {SiNaver, SiKakao} from 'react-icons/si'
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import Nav from "../../components/Nav";
@@ -67,21 +68,35 @@ const Login = () => {
 {/* MDBBtn */}
 {/* MDBBtn color="danger" */}
               <div className="text-center pt-1 mb-5 pb-1">
-                <input type="button" class="custom-button">로그인</input>
+                <button class="custom-button">로그인</button>
+                
                 <div class="text-right">
                     <Link to="/SignUp">회원 가입</Link>
                 </div>
                 <hr/>
-                  <a href="/" class="btn btn-google btn-user btn-block">
+                <a href="/">
+                  <FaGoogle className="google-buttons" color="red"/>
+                </a>
+                <a href="/">
+                  <SiNaver color="Green"/>
+                </a>
+                <a href="/">
+                  <SiKakao color="yellow"/>
+                </a>
+
+                {/* <a class="btn text-white" style="background-color: #dd4b39;" href="#" role="button">
+                    <i class="fab fa-google"></i>
+                  </a> */}
+                  {/* <a href="/" class="btn btn-google btn-user btn-block">
                       <i class="fab fa-google fa-fw"></i>구글
-                  </a>
-                  <a href="/" class="btn btn-naver btn-user btn-block">
+                  </a> */}
+                  {/* <a href="/" class="btn btn-naver btn-user btn-block">
                     <i class="fab fa-facebook-f fa-fw"></i> 네이버
                   </a>
                   <a href="/" class="btn btn-kakao btn-user btn-block">
                     <i class="fab fa-facebook-f fa-fw"></i> 카카오
-                  </a>
-                  <br/><br/>
+                  </a> */}
+                <br/><br/>
                 <a className="text-muted" href="#!">비밀번호를 잃어버리셨나요?</a>
               </div>
 
