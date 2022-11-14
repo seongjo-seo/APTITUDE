d6aefaea5b16e24eace4fa5271ababed455d760b
seoseongjo <tjtjdwh5321@gmail.com>
2022-11-14 18:50:52 +0900

    청첩장 css 추가 및 캘린더 라이브러리 npm

--- client/src/pages/mypage/member/invitation/Invitation.jsx
+++ client/src/pages/mypage/member/invitation/Invitation.jsx
@@ -8,7 +8,7 @@ const Invitation = () => {
       {/* <section className=''> */}
         <div>
           <div className='all-container background-color'></div>
-          <div className='wedding-phrase'> 강동원 ❤ 방구미 <br/>결혼합니다</div>
+          <div className='wedding-phrase'> 방구미 ❤ 강동원 <br/>결혼합니다!</div>
           {/* 첫번째 원 데코레이션 */}
           <div className='one-ellipse-decorate'></div>
           
@@ -26,7 +26,27 @@ const Invitation = () => {
 
           
           <div className='second-rectangular-container'></div>
-          <div className="wedding-photo-image-container"><img src="" alt="..." /></div>
+          {/* <div className="wedding-photo-image-container"> */}
+          <img className='wedding-photo-image-container' src="/img/test_img.jpg" alt="..." />
+          {/* </div> */}
+          
+          <div className='Third-Rectangular-Container-Position'>
+            <div>
+              <div>혼주에게 연락하기</div>
+              <div>신랑 text 들어갈 곳</div>
+              <div>신부 text 들어갈 곳</div>
+            </div>
+          </div>
+          
+          {/* 지도 위 텍스트 */}
+          <div className='Third-text'>&lt;예식장 위치&gt;</div>
+
+          {/* 지도 위치 */}
+          <div className='location-on-the-map'>test</div>
+
+          {/* 지도 아래에 들어갈 텍스트 위치 */}
+          <div className='map-location'>(임시)소피텔 앰배서더 서울 호텔 4층 그랜드볼룸 방돔<br/>(서울 송파구 잠실로 209) 잠실역 도보 10분 거리</div>
+
         </div>
       {/* </section> */}
     </>
