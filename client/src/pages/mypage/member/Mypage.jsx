2b89c6d2ab807c638122fc5bf91e9d47c0860dbd
seoseongjo <tjtjdwh5321@gmail.com>
2022-11-17 11:06:44 +0900

    footer 컴포넌트

--- client/src/pages/mypage/member/Mypage.jsx
+++ client/src/pages/mypage/member/Mypage.jsx
@@ -7,6 +7,7 @@ import {HiOutlineMagnifyingGlass} from "react-icons/hi2";
 import {BsThreeDotsVertical} from "react-icons/bs";
 
 import "./static/sb-admin-2.css";
+import MypageFooter from "./common/MypageFooter";
 // import "./static/sb-admin-2.min.css";
 
 const Mypage = () => {
@@ -676,16 +677,8 @@ const Mypage = () => {
           </div>
           {/* <!-- End of Main Content --> */}
 
-          {/* <!-- Footer --> */}
-          <footer class="sticky-footer bg-white">
-              <div class="container my-auto">
-                  <div class="copyright text-center my-auto">
-                    <span>Copyright 2022 동심원</span>
-                      {/* <span>Copyright &copy; 2022 동심원</span> */}
-                  </div>
-              </div>
-          </footer>
-          {/* <!-- End of Footer --> */}
+          <MypageFooter/>
+
 
       </div>
       {/* <!-- End of Content Wrapper --> */}
@@ -693,27 +686,6 @@ const Mypage = () => {
       </div>
       {/* <!-- End of Page Wrapper --> */}
 
-      {/* <!-- Scroll to Top Button--> */}
-      {/* <a class="scroll-to-top rounded" href="#"><i class="fas fa-angle-up"></i></a> */}
-
-      {/* <!-- Logout Modal--> */}
-      {/* <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-          <div class="modal-content">
-              <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                  <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">x</span>
-                  </button>
-              </div>
-              <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-              <div class="modal-footer">
-                  <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                  <a class="btn btn-primary" href="/">Logout</a>
-              </div>
-          </div>
-      </div>
-      </div> */}
 
         
     </>
---
+++ client/src/pages/mypage/member/common/MypageFooter.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import "../static/sb-admin-2.css"
+
+const MypageFooter = ()=>{
+
+  return (
+    <>
+      {/* <!-- Footer --> */}
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright 2022 동심원</span>
+              {/* <span>Copyright &copy; 2022 동심원</span> */}
+          </div>
+        </div>
+      </footer>
+      {/* <!-- End of Footer --> */}
+    </>
+  );
+}
+
+export default MypageFooter;
