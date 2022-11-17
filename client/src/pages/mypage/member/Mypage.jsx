ddcdbc10157927f8e9f4a6f512822e19df3289f1
seoseongjo <tjtjdwh5321@gmail.com>
2022-11-17 15:09:49 +0900

    mypage css 불필요 코드 전체 제거

--- client/src/pages/mypage/member/Mypage.jsx
+++ client/src/pages/mypage/member/Mypage.jsx
@@ -17,7 +17,7 @@ const Mypage = () => {
     
     
     <section>
-      <MypageNav/>
+      {/* <MypageNav/> */}
 
 
       {/* 뒷 배경 색상 코어 잡는 곳. */}
@@ -28,7 +28,7 @@ const Mypage = () => {
       {/* Content Wrapper */}
       <div id="content-wrapper" class="d-flex flex-column">
 
-      {/* 상당 header 시작 위치  */}
+      {/* 상단 header 시작 위치  */}
       <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
       {/* <!-- Topbar Search --> */}
@@ -46,47 +46,28 @@ const Mypage = () => {
 
 
 
-    {/* <!-- Topbar Navbar --> */}
+    {/* 상단 헤더 바 위치 */}
     <ul class="navbar-nav ml-auto">
-
-        {/* <!-- Nav Item - Search Dropdown (Visible Only XS) --> */}
+        {/*  Nav Item - Search Dropdown (Visible Only XS) */}
         <li class="nav-item dropdown no-arrow d-sm-none">
-            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            {/* <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="fas fa-search fa-fw"></i>
-            </a>
-            {/* <!-- Dropdown - Messages --> */}
-            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                aria-labelledby="searchDropdown">
-                <form class="form-inline mr-auto w-100 navbar-search">
-                    <div class="input-group">
-                        <input type="text" class="form-control bg-light border-0 small"
-                            placeholder="Search for..." aria-label="Search"
-                            aria-describedby="basic-addon2"/>
-                        <div class="input-group-append">
-                            <button class="btn btn-primary" type="button">
-                                {/* <i class="fas fa-search fa-sm"></i> */}
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </li>
+            </a> */}
+            
+          </li>
 
-        {/* <!-- Nav Item - Alerts --> */}
+        {/* 상단헤더 내비에 있는 벨 */}
         <li class="nav-item dropdown no-arrow mx-1">
-            <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <Link class="nav-link dropdown-toggle" to={"#"} id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <BiBell style={{width:"22", height:"22", viewBox:"0 0 22 22"}}/>
                 {/* <i class="fas fa-bell fa-fw"></i> */}
                 {/* <!-- Counter - Alerts --> */}
-                <span class="badge badge-danger badge-counter">3+</span>
-            </a>
-            {/* <!-- Dropdown - Alerts --> */}
-            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                <span class="badge badge-danger badge-counter">5+</span>
+            </Link>
+            {/* 알람 내부 시작 영역 */}
+            {/* <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
                 aria-labelledby="alertsDropdown">
-                <h6 class="dropdown-header">
-                    Alerts Center
-                </h6>
+                <h6 class="dropdown-header">Alerts Center</h6>
                 <a class="dropdown-item d-flex align-items-center" href="#">
                     <div class="mr-3">
                         <div class="icon-circle bg-primary">
@@ -105,8 +86,7 @@ const Mypage = () => {
                         </div>
                     </div>
                     <div>
-                        <div class="small text-gray-500">December 7, 2019</div>
-                        $290.29 has been deposited into your account!
+                        <div class="small text-gray-500">December 7, 2019</div>$290.29 has been deposited into your account!
                     </div>
                 </a>
                 <a class="dropdown-item d-flex align-items-center" href="#">
@@ -121,10 +101,11 @@ const Mypage = () => {
                     </div>
                 </a>
                 <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
-            </div>
+            </div> */}
+            {/* 알람 내부 종료 영역  */}
         </li>
 
-        {/* Nav Item - Messages */}
+        {/* 내비 내부 메시지 정리  nav messages info */}
         <li class="nav-item dropdown no-arrow mx-1">
             <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -132,58 +113,9 @@ const Mypage = () => {
                 {/* <i class="fas fa-envelope fa-fw">여기</i> */}
 
                 {/* 메시지 개수 */}
-                <span class="badge badge-danger badge-counter">7</span>
+                <span class="badge badge-danger badge-counter">2</span>
             </a>
-            {/* <!-- Dropdown - Messages --> */}
-            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                aria-labelledby="messagesDropdown">
-                <h6 class="dropdown-header">Message Center</h6>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="dropdown-list-image mr-3">
-                        <img class="rounded-circle" src="" alt="..."/>
-                        <div class="status-indicator bg-success"></div>
-                    </div>
-                    <div class="font-weight-bold">
-                        <div class="text-truncate">Hi there! I am wondering if you can help me with a
-                            problem I've been having.</div>
-                        <div class="small text-gray-500">Emily Fowler · 58m</div>
-                    </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="dropdown-list-image mr-3">
-                        <img class="rounded-circle" src="" alt="..."/>
-                        <div class="status-indicator"></div>
-                    </div>
-                    <div>
-                        <div class="text-truncate">I have the photos that you ordered last month, how
-                            would you like them sent to you?</div>
-                        <div class="small text-gray-500">Jae Chun · 1d</div>
-                    </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="dropdown-list-image mr-3">
-                        <img class="rounded-circle" src="" alt="..."/>
-                        <div class="status-indicator bg-warning"></div>
-                    </div>
-                    <div>
-                        <div class="text-truncate">Last month's report looks great, I am very happy with
-                            the progress so far, keep up the good work!</div>
-                        <div class="small text-gray-500">Morgan Alvarez · 2d</div>
-                    </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="dropdown-list-image mr-3">
-                        <img class="rounded-circle" src="" alt="..."/>
-                        <div class="status-indicator bg-success"></div>
-                    </div>
-                    <div>
-                        <div class="text-truncate">Am I a good boy? The reason I ask is because someone
-                            told me that people say this to all dogs, even if they aren't good...</div>
-                        <div class="small text-gray-500">Chicken the Dog · 2w</div>
-                    </div>
-                </a>
-                <a class="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
-            </div>
+           
         </li>
 
         <div class="topbar-divider d-none d-sm-block"></div>
